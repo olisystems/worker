1498a7963f7105cc90461bf7b8633a2133acd79e
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-04-05 14:03:15 +0200

    [cli/demo_energy_market] successfully run the full demo.

--- cli/demo_energy_market.sh
+++ cli/demo_energy_market.sh
@@ -23,7 +23,7 @@ set -euo pipefail
 # usage:
 #  demo_energy_market.sh -p <NODEPORT> -P <WORKERPORT> -t -O <path-to-order-file>
 
-while getopts ":p:P:u:V:C:O:T:" opt; do
+while getopts ":p:P:u:V:C:A:O:T:" opt; do
     case $opt in
         p)
             NPORT=$OPTARG
@@ -39,6 +39,9 @@ while getopts ":p:P:u:V:C:O:T:" opt; do
             ;;
         C)
             CLIENT_BIN=$OPTARG
+            ;;
+        A)
+            ACTOR_ID=$OPTARG
             ;;
         O)
             ORDERS_FILE=$OPTARG
@@ -62,9 +65,11 @@ WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
 
 CLIENT_BIN=${CLIENT_BIN:-"./../bin/integritee-cli"}
 
-# Timestamp needs to match the one from the provided orders file
+# Timestamp needs to match the one from the provided orders file.
+# Otherwise, you will get a results/proof not found error.
 TIMESTAMP=${TIMESTAMP:-"2022-03-04T05:06:07+00:00"}
 ORDERS_FILE=${ORDERS_FILE:-"../bin/orders/order_10_users.json"}
+ACTOR_ID=${ACTOR_ID:-"actor_0"}
 
 echo "Using client binary ${CLIENT_BIN}"
 echo "Using node uri ${NODEURL}:${NPORT}"
@@ -72,6 +77,7 @@ echo "Using trusted-worker uri ${WORKER1URL}:${WORKER1PORT}"
 echo ""
 echo "Using ORDERS_FILE: ${ORDERS_FILE}"
 echo "Using TIMESTAMP: ${TIMESTAMP}"
+echo "Using ACTOR_ID: ${ACTOR_ID}"
 echo ""
 
 FILE_CONTENTS=$(cat "$ORDERS_FILE" | tr -d '[:space:]')
@@ -81,8 +87,16 @@ ORDERS_STRING="$FILE_CONTENTS"
 CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
 read -r MRENCLAVE <<< "$($CLIENT list-workers | awk '/  MRENCLAVE: / { print $2; exit }')"
 
+echo "* Executing pay-as-pid market algorithm"
+RESULT=`$CLIENT trusted --mrenclave ${MRENCLAVE} --direct pay-as-bid //Alice ${ORDERS_STRING}`
+echo "Result: ${RESULT}"
+
+#echo "* Getting market results market algorithm"
+#MARKET_RESULT=`$CLIENT trusted --mrenclave ${MRENCLAVE} --direct get-market-results //Alice ${TIMESTAMP}`
+#echo "Proof: ${MARKET_RESULT}"
+
 echo "* Getting merkle proof for orders"
-PROOF=`$CLIENT trusted --mrenclave ${MRENCLAVE} --direct pay-as-bid-proof //Alice ${TIMESTAMP} actor_0`
+PROOF=`$CLIENT trusted --mrenclave ${MRENCLAVE} --direct pay-as-bid-proof //Alice ${TIMESTAMP} ${ACTOR_ID}`
 echo "Proof: ${PROOF}"
 
 echo "* Verifying merkle proof"
